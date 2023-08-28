182d30434f8a0bd4e9c5b93247650055895641ee
djkim12 <djkim12@douzone.com>
2023-08-28 19:50:39 +0900

    3.7 빈 상속 결정하기 마침.

---
+++ src/main/java/com/apress/prospring5/ch3/sec7/xml/Singer.java
@@ -0,0 +1,23 @@
+package com.apress.prospring5.ch3.sec7.xml;
+
+public class Singer {
+
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Singer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
