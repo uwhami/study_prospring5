89a4f182f22b52635e8d0c2657e33e253faa59b7
dajeong <uwhami@gmail.com>
2023-09-21 00:11:49 +0900

    6.9.2 Jdbc Template를 사용해 단일 값 조회하기(에러나는중...)

--- src/main/java/com/apress/prospring5/ch6/sec8/JdbcSingerDao.java
+++ src/main/java/com/apress/prospring5/ch6/sec8/JdbcSingerDao.java
@@ -2,6 +2,7 @@ package com.apress.prospring5.ch6.sec8;
 
 import com.apress.prospring5.ch6.dao.SingerDao;
 import com.apress.prospring5.ch6.entity.Singer;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -51,9 +52,18 @@ public class JdbcSingerDao implements SingerDao, InitializingBean {
         return null;
     }
 
+    /**
+     * 6.9.2 JdbcTemplate를 사용해 단일 값 조회하기.
+     * 예제 6-33. findNameById 메서드 추가.
+     * 첫번째인자 - SQL 구문
+     * 두번쨰인자 - 파라미터에 바인딩할 값을 Object 배열 형태로 지정
+     * 세번쟤인자 - 반환 값의 타입 지정.
+     */
     @Override
     public String findNameById(Long id) {
-        return null;
+        return jdbcTemplate.queryForObject(
+          "select first_name || ' ' || last_name from singer where id = ?",
+          new Object[]{id}, String.class);
     }
 
     @Override
@@ -68,7 +78,7 @@ public class JdbcSingerDao implements SingerDao, InitializingBean {
 
     @Override
     public void insert(Singer singer) {
-
+        throw new NotImplementedException("insert");
     }
 
     @Override
