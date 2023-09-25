52fbb1e4e73cc474de8779f334bc9aaeacc8f08c
dajeong <uwhami@gmail.com>
2023-09-25 23:22:31 +0900

    6.9.4 RowMapper<T>를 사용해 도메인 객체 조회하기.

---
+++ src/main/java/com/apress/prospring5/ch6/sec9/RowMapperTest.java
@@ -0,0 +1,47 @@
+package com.apress.prospring5.ch6.sec9;
+
+import com.apress.prospring5.ch6.dao.SingerDao;
+import com.apress.prospring5.ch6.entity.Album;
+import com.apress.prospring5.ch6.entity.Singer;
+import com.apress.prospring5.ch6.sec9.config.NamedJdbcCfg;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+public class RowMapperTest {
+
+    @Test
+    public void testRowMapper(){
+        GenericApplicationContext ctx = new AnnotationConfigApplicationContext(NamedJdbcCfg.class);
+        SingerDao dao = ctx.getBean(SingerDao.class);
+        assertNotNull(dao);
+        List<Singer> singers = dao.findAll();
+        assertTrue(singers.size() == 3);
+
+        singers.forEach(singer -> {
+            System.out.println(singer);
+            if(singer.getAlbums() != null){
+                for(Album album : singer.getAlbums()){
+                    System.out.println("==========" + album);
+                }
+            }
+        });
+
+        System.out.println("==========");
+
+        singers = dao.findByFirstName("John");
+        singers.forEach(singer -> {
+            System.out.println(singer);
+            if(singer.getAlbums() != null){
+                for(Album album : singer.getAlbums()){
+                    System.out.println("==========" + album);
+                }
+            }
+        });
+    }
+}
