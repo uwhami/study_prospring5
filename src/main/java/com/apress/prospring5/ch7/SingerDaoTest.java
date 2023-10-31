1a55a0a343755bc40e4e95839eea565320caaaf4
dajeong <uwhami@gmail.com>
2023-10-31 23:02:11 +0900

    7.5 Data Register

---
+++ src/main/java/com/apress/prospring5/ch7/SingerDaoTest.java
@@ -0,0 +1,71 @@
+package com.apress.prospring5.ch7;
+
+import com.apress.prospring5.ch7.config.AppConfig;
+import com.apress.prospring5.ch7.dao.SingerDao;
+import com.apress.prospring5.ch7.entities.Album;
+import com.apress.prospring5.ch7.entities.Singer;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import static com.apress.prospring5.ch7.SpringHibernateDemo.listSingersWithAlbum;
+import static org.junit.Assert.assertNotNull;
+
+public class SingerDaoTest {
+
+    private static Logger logger = LoggerFactory.getLogger(SingerDaoTest.class);
+
+    private GenericApplicationContext ctx;
+    private SingerDao singerDao;
+
+    @Before
+    public void setUp(){
+        ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        singerDao = ctx.getBean(SingerDao.class);
+        assertNotNull(singerDao);
+    }
+
+    @Test
+    public void testInsert(){
+        Singer singer = new Singer();
+        singer.setFirstName("BB");
+        singer.setLastName("King");
+        singer.setBirthDate(new Date((new GregorianCalendar(1940, 8, 16)).getTime().getTime()));
+
+        Album album = new Album();
+        album.setTitle("My Kind of Blues");
+        album.setReleaseDate(new java.sql.Date((new GregorianCalendar(1961,7,18)).getTime().getTime()));
+        singer.addAlbum(album);
+
+        album = new Album();
+        album.setTitle("A Heart Full of Blues");
+        album.setReleaseDate(new java.sql.Date((new GregorianCalendar(1962,3,20)).getTime().getTime()));
+        singer.addAlbum(album);
+
+        singerDao.save(singer);
+        assertNotNull(singer.getId());
+
+        List<Singer> singers = singerDao.findAllWithAlbum();
+        listSingersWithAlbum(singers);
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Singer> singers = singerDao.findAll();
+        listSingersWithAlbum(singers);
+    }
+
+    @Test
+    public void testFindById(){
+        Singer singer = singerDao.findById(1L);
+        logger.info("==========" + singer.toString());
+    }
+
+}
