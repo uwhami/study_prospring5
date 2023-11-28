4e5ebb074b4c188af238debf8ae80c609fb78d3e
djkim12 <djkim12@douzone.com>
2023-11-28 16:13:56 +0900

    10.5.1 Spring Validator Interface

---
+++ src/main/java/com/apress/prospring5/ch10/sec5/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.apress.prospring5.ch10.sec5.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.apress.prospring5.ch10.sec5")
+public class AppConfig {
+}
